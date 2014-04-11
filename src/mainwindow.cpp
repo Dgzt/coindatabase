0d8d61aef41529c0f4a387844b6db2957fcb7f31
Zsuró Tibor <zsurotibor@gmail.com>
2014-04-11 08:37:05 +0200

    Add quit menu for menubar.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,7 +1,22 @@
 #include "QDebug"
+#include "QAction"
+#include "QMenu"
+#include "QMenuBar"
 #include "mainwindow.h"
 
 MainWindow::MainWindow()
 {
     qDebug()<< "Init.";
+
+    createMenu();
+}
+
+void MainWindow::createMenu()
+{
+    QAction *quitAction = new QAction(tr("Quit"), this);
+
+    connect(quitAction, SIGNAL( triggered() ), this, SLOT( close() ) );
+
+    QMenu *fileMenu = menuBar()->addMenu("File");
+    fileMenu->addAction( quitAction );
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,8 +4,15 @@
 #include <QMainWindow>
 
 class MainWindow : public QMainWindow{
+
 public:
     MainWindow();
+
+private:
+    /*!
+     * Create the menu.
+     */
+    void createMenu();
 };
 
 #endif // MAINWINDOW_H
