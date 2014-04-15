3dd500dc029be91f93ec63d4e713bf86e3463e92
Zsuró Tibor <zsurotibor@gmail.com>
2014-04-15 19:29:28 +0200

    Create countries table when this is not exists.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,6 +4,8 @@
 #include "QMenuBar"
 #include "QDir"
 #include "QtSql/QSqlDatabase"
+#include "QtSql/QSqlQuery"
+#include "QtSql/QSqlError"
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -48,6 +50,26 @@ void MainWindow::checkLocalFiles()
 
     if( localDb.open() ){
         qDebug() << "Open.";
+
+        if( !localDb.tables().contains("countries") ){
+
+            QSqlQuery createCountriesQuery(localDb);
+            bool ok = createCountriesQuery.exec( QString("CREATE TABLE countries (")+
+                                                    QString("id               INTEGER PRIMARY KEY,")+
+                                                    QString("id_server        BIGINT,")+
+                                                    QString("name             VARCHAR(64) NOT NULL,")+
+                                                    QString("modify_date      DATETIME NOT NULL")+
+                                                 QString(")"));
+
+            if(ok){
+                qDebug() << "Ok create countries table";
+            }else{
+                qDebug() << "Not ok create countries table";
+                qDebug() << createCountriesQuery.lastError().text();
+            }
+
+        }
+
     }else{
         qDebug() << "not open.";
     }
