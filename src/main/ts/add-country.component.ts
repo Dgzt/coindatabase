071e10a14dbea4c315000cdf09150bd5033c6bee
Zsuró Tibor <zsurotibor@gmail.com>
2016-03-23 20:50:48 +0100

    When the country was successfully saved then go to countries page.
    Change-Id: I51935cf84b04780b946af4313ba9ada91f0f49ee

--- src/main/ts/add-country.component.ts
+++ src/main/ts/add-country.component.ts
@@ -1,5 +1,6 @@
 import { Component }    from 'angular2/core';
 import { NgForm }       from 'angular2/common';
+import { Router } from 'angular2/router';
 
 import { CountryService } from './country.service';
 import { Country }      from './country';
@@ -13,11 +14,24 @@ export class AddCountryComponent {
     
     model : Country = {name: ''};
     
-        constructor(
+    constructor(
+        private _router: Router,
         private _countryService : CountryService
     ){ }
     
+    /**
+     * Go to countries page.
+     */
+    gotoCountries(){
+        this._router.navigate(['Countries']);
+    }
+    
+    /**
+     * When the country is successfully save then return go to countries page.
+     */
     onSubmit() {
-        this._countryService.save(this.model).subscribe((res: Country) => this.model = res);
+        this._countryService.save(this.model).subscribe((res: Country) => {
+            this.gotoCountries();
+        });
     }
 }
